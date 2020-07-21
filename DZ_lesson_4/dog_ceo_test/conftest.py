709785a19148aa94f639dadc9601a85482fcb0cf
Alex Kulikova <antares.alexx@gmail.com>
2020-07-22 00:14:59 +0300

    DZ_4 c 1 пулом тестов по сайту dog.ceo/api

--- DZ_lesson_4/dog_ceo_test/conftest.py
+++ DZ_lesson_4/dog_ceo_test/conftest.py
@@ -1,13 +1,23 @@
-# import pytest
-# import random
+import pytest
+import requests
+import json
 
-# #Создаем список для тестирования
-# #Получаем список длинной 5
-# @pytest.fixture
-# def fixture_dog_breed_random():
-# 	random_breed = #рандомно подставляем породу из списка
-# 	return random_breed
+@pytest.fixture()
+def fixture_dog_api_url():
+    return 'https://dog.ceo/api/'
 
-# # @pytest.fixture(params=[1, 4, 2, 6])
-# # def fixture_with_params(request):
-# #     return request.param
+# Получение количества всех картинок по указанной подпороде LIST ALL SUB-BREED IMAGES
+@pytest.fixture()
+def fixture_number_all_subbreed_image(test_input, expected_result, fixture_dog_api_url):
+    if len(expected_result) != 0:
+        url = fixture_dog_api_url+f'breed/'+str(test_input)+'/'+str(expected_result[0])+f'/images'
+        responce = requests.get(url)
+        if responce.status_code == 200:
+            get_responce = responce.json()
+        else:
+            print('Server is not responding now')
+        if not get_responce:
+            raise ValueError('Get_responce in not loaded')
+        list_image_multiple_subbreed = get_responce.get('message')
+        number_image_subbreed = len(list_image_multiple_subbreed)
+        return number_image_subbreed
--- DZ_lesson_4/dog_ceo_test/test_dog_ceo.py
+++ DZ_lesson_4/dog_ceo_test/test_dog_ceo.py
@@ -2,45 +2,81 @@ import pytest
 import requests
 import json
 
-#Тестируем получение json со всеми породами LIST ALL BREEDS
-#https://dog.ceo/api/breeds/list/all
-#записываем его в json breeds.json
-def test_all_breeds_list():
-	url = 'https://dog.ceo/api/breeds/list/all'
-	responce = requests.get(url=url)
-	with open('breeds.json', 'w') as breed_data:
-		responce = json.dumps(responce, indent=4)
-		breed_data.write(responce)
-	assert responce.status_code == 200
-
-#Тестируем получение SINGLE RANDOM IMAGE FROM ALL DOGS
-
-#Тестируем получение всех картинок по указанной породе
+#Тестируем получение json со всеми породами LIST ALL BREEDS, проверяем статус код на 200
+#Функция скачивает через API данные и сохраняет их в json, используем generate_test
+def pytest_generate_tests(metafunc):
+    if 'test_input' not in metafunc.fixturenames:
+        return
+    url = 'https://dog.ceo/api/breeds/list/all'
+    responce = requests.get(url)
+    if responce.status_code == 200:
+        get_responce = responce.json()
+    else:
+        print('Server is not responding now')
+    if not get_responce:
+        raise ValueError('Get_responce in not loaded')
+    breed_dict = get_responce.get('message')
+    breed_subbreed = breed_dict.items()
+    with open('breeds.json', 'w') as breed_data:
+        get_responce = json.dumps(get_responce, indent=4)
+        breed_data.write(get_responce)
+    return metafunc.parametrize('test_input, expected_result', breed_subbreed)
 
 #Тестируем получение LIST ALL SUB-BREEDS по запрашиваемой породе
+def test_list_all_subbreeds(test_input, expected_result, fixture_dog_api_url):
+    url = fixture_dog_api_url+f'breed/'+str(test_input)+f'/list'
+    responce = requests.get(url)
+    if responce.status_code == 200:
+        get_responce = responce.json()
+    else:
+        print('Server is not responding now')
+    if not get_responce:
+        raise ValueError('Get_responce in not loaded')
+    list_all_subbreeds = get_responce.get('message')
+    assert list_all_subbreeds == expected_result
 
-#Проверка рандомного получения фото из породы BREEDS LIST
-# def test_breed_random(fixture_dogceo_random):
-# 	url = f'https://dog.ceo/api/breed/{fixture_dogceo_random}/images/random'
-# 	responce = requests.get(url=url)
-# 	assert responce.status_code == 200
+# Тестируем получение MULTIPLE IMAGES FROM A SUB-BREED COLLECTION
+def test_multiple_image_subbreed(test_input, expected_result, fixture_dog_api_url, fixture_number_all_subbreed_image):
+    if len(expected_result) != 0:
+        url = fixture_dog_api_url+f'breed/'+str(test_input)+f'/'+str(expected_result[0])+f'/images/random/'+str(fixture_number_all_subbreed_image)
+        responce = requests.get(url)
+        if responce.status_code == 200:
+            get_responce = responce.json()
+        else:
+            print('Server is not responding now')
+        if not get_responce:
+            raise ValueError('Get_responce in not loaded')
+        list_image_multiple_subbreed = get_responce.get('message')
+        assert len(list_image_multiple_subbreed) == int(fixture_number_all_subbreed_image)
+    else:
+        print('Breed don\'t have any sub-breed')
 
-# #Тестируем метод c параметризацией
-# #Тестируем метод sort у list
-# #Получаем на вход список длинной 5
-# @pytest.mark.parametrize('x', [1, 4, 9, 7])
-# def test_sort_element(fixture_someone_list, x):
-# 	fixture_someone_list.append(x)
-# 	fixture_someone_list.sort()
-# 	min_number = min(fixture_someone_list)
-# 	max_number = max(fixture_someone_list)
-# 	assert fixture_someone_list.index(min_number) == 0
-# 	assert fixture_someone_list.index(max_number) == 5
+#Тестируем получение DISPLAY MULTIPLE RANDOM IMAGES FROM ALL DOGS COLLECTION 
+#Рандомно перебираем числа до 50 и сравниваем полученное число элементов в листе в значении (должно совпадать))
+@pytest.mark.parametrize('number', [1, 4, 9, 7, 30, 50, 49, 25])
+def test_breeds_image_random(fixture_dog_api_url, number):
+    url = fixture_dog_api_url+f'breeds/image/random/'+str(number)
+    responce = requests.get(url)
+    if responce.status_code == 200:
+        get_responce = responce.json()
+    else:
+        print('Server is not responding now')
+    if not get_responce:
+        raise ValueError('Get_responce in not loaded')
+    list_image_random = get_responce.get('message')
+    assert len(list_image_random) == int(number)
 
-# #Тестируем метод c параметризацией
-# #Тестируем метод clear у list
-# #Получаем на вход список длинной 5 и добавляем разные значения до очистки
-# def test_clear_element(fixture_someone_list, fixture_with_params):
-# 	fixture_someone_list.append(fixture_with_params)
-# 	fixture_someone_list.clear()
-# 	assert len(fixture_someone_list) == 0
+#Тестируем получение DISPLAY SINGLE RANDOM IMAGE FROM ALL DOGS COLLECTION
+def test_breeds_image_random_one(fixture_dog_api_url):
+    url = fixture_dog_api_url+f'breeds/image/random/'
+    responce = requests.get(url)
+    if responce.status_code == 200:
+        get_responce = responce.json()
+    else:
+        print('Server is not responding now')
+    if not get_responce:
+        raise ValueError('Get_responce in not loaded')
+    image_random = get_responce.get('message')
+    jpg_symbol = ['.jpg']
+    coincidental = [e for e in jpg_symbol if e in image_random]
+    assert coincidental == jpg_symbol
