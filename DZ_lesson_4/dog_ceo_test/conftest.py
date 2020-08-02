1a7eb8f16a8ea2380ae736f0541ff08ce023582b
Alex Kulikova <antares.alexx@gmail.com>
2020-08-02 13:10:02 +0300

    Выполненное задание 2 по тестированиею api.openbrewerydb.org и внесены доп пробелы в задание 1 по руководству PIP 8

--- DZ_lesson_4/dog_ceo_test/conftest.py
+++ DZ_lesson_4/dog_ceo_test/conftest.py
@@ -2,15 +2,17 @@ import pytest
 import requests
 import json
 
+
 @pytest.fixture()
 def fixture_dog_api_url():
     return 'https://dog.ceo/api/'
+
 
 # Получение количества всех картинок по указанной подпороде LIST ALL SUB-BREED IMAGES
 @pytest.fixture()
 def fixture_number_all_subbreed_image(test_input, expected_result, fixture_dog_api_url):
     if len(expected_result) != 0:
-        url = fixture_dog_api_url+f'breed/'+str(test_input)+'/'+str(expected_result[0])+f'/images'
+        url = fixture_dog_api_url + f'breed/' + str(test_input) + '/' + str(expected_result[0]) + f'/images'
         responce = requests.get(url)
         if responce.status_code == 200:
             get_responce = responce.json()
