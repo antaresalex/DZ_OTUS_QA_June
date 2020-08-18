3db8814417bc070b80811415a8c2eeb3c62d86a2
Alex Kulikova <antares.alexx@gmail.com>
2020-08-19 01:11:18 +0300

    Написан тест разлогинивания администратора

--- DZ_lesson_8/test_opencart_admin_login_page.py
+++ DZ_lesson_8/test_opencart_admin_login_page.py
@@ -1,7 +1,7 @@
 # Тест страницы логина в админку /admin/
 
 # Тестируем логин уже существующего пользователя
-def test_login_user(browser, url):
+def test_login_admin(browser, url):
     admin_login_page_url = url + f'/admin/'
     browser.get(admin_login_page_url)
     user_name = 'user'
@@ -17,7 +17,7 @@ def test_login_user(browser, url):
 
 
 # Тестируем логин несуществующего пользователя
-def test_login_incorrect_user(browser, url):
+def test_login_incorrect_admin(browser, url):
     admin_login_page_url = url + f'/admin/'
     browser.get(admin_login_page_url)
     user_name = 'gggg'
@@ -28,3 +28,23 @@ def test_login_incorrect_user(browser, url):
     alert_danger = browser.find_element_by_css_selector('.alert-dismissible')
     displayed_alert = alert_danger.is_displayed()
     assert displayed_alert is True
+
+
+# Тестирование разлогина раздела администратора
+def test_logout_admin(browser, url):
+    admin_login_page_url = url + f'/admin/'
+    browser.get(admin_login_page_url)
+    user_name = 'user'
+    user_password = 'bitnami1'
+    browser.find_element_by_css_selector('#input-username').send_keys(user_name)
+    browser.find_element_by_css_selector('#input-password').send_keys(user_password)
+    browser.find_element_by_css_selector('[type="submit"]').click()
+    browser.find_element_by_css_selector('.fa-caret-down').click()
+    browser.find_element_by_css_selector('.navbar-nav li:nth-child(2) a').click()
+    enter_login_info = browser.find_element_by_css_selector('.panel-title').text
+    assert enter_login_info == 'Please enter your login details.'
+
+
+# Добавить явные ожидания
+# Реализовать 2 тестовых сценария на раздел администратора
+# Добавить проверку перехода к разделу с товарами, что появляется таблица с товарами.
