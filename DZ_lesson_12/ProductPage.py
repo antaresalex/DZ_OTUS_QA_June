6336ae4291cf4d8c8ebca04d81d5023783683f77
Alex Kulikova <antares.alexx@gmail.com>
2020-09-10 00:10:52 +0300

    Дописано выделение в коде общих этапов для Pages

--- DZ_lesson_12/ProductPage.py
+++ DZ_lesson_12/ProductPage.py
@@ -4,15 +4,10 @@ class ProductPage:
 # product_page_url = url + f'/index.php?route=product/product&path=57&product_id='
 
 # получаем название товара на странице из заголовка
-
 # нажимаем на добавить в виш лист add_to_wish_list
-
 # нажимаем на добавить в корзину add_to_cart
-
 # открываем написание ревью open_review
 # отправляем ревью send_review
-
 # получаем имя продукта на странице
-
 # получаем стоимость продукта на Product Page
 
--- DZ_lesson_12/test_opencart_catalog_page.py
+++ DZ_lesson_12/test_opencart_catalog_page.py
@@ -8,21 +8,28 @@ from selenium.webdriver.support import expected_conditions as EC
 # Тест добавления в сравнение Compare
 @pytest.mark.parametrize("product_index", [1, 5, 11])
 def test_add_to_compare(browser, url, product_index, wait):
+    # открываем каталог
     catalog_page_url = url + f'/index.php?route=product/category&path=20'
     browser.get(catalog_page_url)
     first_product = browser.find_elements_by_css_selector('.product-grid')[0]
+    # перематываем страницу до элемента
     first_product.location_once_scrolled_into_view
+    # добавляем товар в сравнение
     compare_button = first_product.find_element_by_css_selector('[data-original-title="Compare this Product"]')
     compare_button.click()
     second_product = browser.find_elements_by_css_selector('.product-grid')[product_index]
+    # перематываем страницу до элемента
     second_product.location_once_scrolled_into_view
     second_product_name_js = str(
         f'return document.querySelectorAll(".product-layout .img-responsive")[' + str(product_index) + f'].title;')
     second_product_name = browser.execute_script(second_product_name_js)
+    # добавляем товар в сравнение
     second_compare_button = second_product.find_element_by_css_selector('[data-original-title="Compare this Product"]')
     second_compare_button.click()
+    # переходим в список сравнения выбранных товаров
     button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#compare-total')))
     button.click()
+    # страница сравненения товаров, получаем название товара в списке
     wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '#content tbody tr strong')))
     second_compare_product = browser.find_elements_by_css_selector('#content tbody tr strong')[1].text
     assert second_compare_product == second_product_name
@@ -31,10 +38,14 @@ def test_add_to_compare(browser, url, product_index, wait):
 # Тест клика по #column-left > div.list-group на Desktops что мы точно на Desktops по тексту #content > h2
 @pytest.mark.parametrize("catalog_id", [20, 34, 18])
 def test_catalog_name(browser, url, catalog_id, wait):
+    # открываем каталог
     catalog_page_url = url + f'/index.php?route=product/category&path=' + str(catalog_id)
     browser.get(catalog_page_url)
+    # получаем название раздела из залоговка каталога
     catalog_name = browser.find_element_by_css_selector('#content h2').text
+    # переходим в раздел каталога из меню-списка слева
     other_catalog_list = browser.find_element_by_css_selector('.list-group')
     other_catalog_list.find_element_by_partial_link_text(catalog_name).click()
+    # получаем название раздела из залоговка каталога
     new_catalog_name = browser.find_element_by_css_selector('#content h2').text
     assert new_catalog_name == catalog_name
